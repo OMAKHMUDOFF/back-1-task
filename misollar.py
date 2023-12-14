101fb3cd70352934cac94d8742e0653f3984a16b
OMAKHMUDOFF <omaxmudovv@gmail.com>
2023-12-14 21:21:17 +0500

    missolar yechildi

---
+++ misollar.py
@@ -0,0 +1,47 @@
+friendName = "Akmal"
+print(f"{friendName} do'stim sen bilan 15 yildan beru do'st ekanmiz")
+print("----------------------------")
+num = 50
+print(f"{num} 30 dan kattami?")
+print("----------------------------")
+firstPerson, firstPersonAge = "Akmal", 18
+secondPerson, secondPersonAge = "Rayxona", 22
+print(
+    f"{secondPerson}, {firstPerson} dan {secondPersonAge - firstPersonAge} yoshga katta"
+)
+print("----------------------------")
+firstNum, secondNum = 10, 10
+thirdNum = firstNum + secondNum
+print(thirdNum * 30)
+print("----------------------------")
+a, b, c, d, e, f = 15, 22, 28, 31, 17, 25
+p = a + b + c + d + e + f
+print(f"6 burchakning perimetri {p} ga teng")
+print("----------------------------")
+num1, num2 = 14, 2
+division = num1 / num2
+print(f"{num1}, {num2} bo'linmasining ma'lumot turi {type(division) , division}")
+print("----------------------------")
+trainSpeed = 100
+boatSpeed = 35
+carSpeed = 150
+pedestrianSpeed = carSpeed - (trainSpeed + boatSpeed)
+print(f"Piyodaning tezligi: {pedestrianSpeed} km/s")
+print("----------------------------")
+year_of_birth = 2006
+CurrentYear = 2023
+print(
+    f"Sobir {year_of_birth} yilda tug'ilgan va u {CurrentYear - year_of_birth} yoshda!"
+)
+print("----------------------------")
+# 4 больше 2 на 7
+# а 2 больше 5 на 3
+# 4 больше 5 на х
+print(f"4-savol 5-savoldan {7 - 3} marta qiyin")
+print("----------------------------")
+hours, minutes = 10, 45
+rolton = 7
+hotlanch = 2
+print(
+    f"Anvar bilan Sobir taomlariga {hours}:{minutes} suv quyishdi, Anvarning ovqati {hours}:{minutes + hotlanch}, Sobirning ovqati esa {hours}:{minutes + rolton} "
+)
